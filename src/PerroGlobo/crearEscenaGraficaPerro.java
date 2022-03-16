deb4a9458b41fbc4db10dc54217f8ac8733bc2c6
DE LA CRUZ VILLANUEV <DE LA CRUZ VILLANUEV@LAPTOP-5JI9OPV6>
2022-03-15 23:06:33 -0600

    Clase capsula y reforma del còdigo de escena gràfica

--- src/PerroGlobo/crearEscenaGraficaPerro.java
+++ src/PerroGlobo/crearEscenaGraficaPerro.java
@@ -23,68 +23,41 @@ class crearEscenaGraficaPerro {
     
     public crearEscenaGraficaPerro(){
         
-        Appearance apBlanca = new Appearance();
-        //<-----------CUERPO----------->
-        //Panza
-        Transform3D t3dPanza = new Transform3D();
-        t3dPanza.rotZ(Math.PI/180*90);
-        TransformGroup tgPanza = new TransformGroup(t3dPanza);
-        Cylinder panza = new Cylinder(0.2f, 0.6f,apBlanca);
-        objRaiz.addChild(tgPanza);
-        tgPanza.addChild(panza);
+        capsula panza = new capsula(1,1);
+        capsula cabeza = new capsula(0.4f,0.27f);
+        capsula cola1 = new capsula(0.3f,0.15f);
+        capsula cola2 = new capsula(0.1f,0.3f);
         
-        //Esfera 1
-        Transform3D t3dEsferaPanza1 = new Transform3D();
-        t3dEsferaPanza1.setTranslation(new Vector3f(0.0f,0.3f,0.0f));
+        objRaiz.addChild(panza.tgCapsula);
+        objRaiz.addChild(cabeza.tgCapsula);
+        panza.tgCapsula.addChild(cola1.tgCapsula);
+        cola1.tgCapsula.addChild(cola2.tgCapsula);
+        //<------CABEZA----->
+        Transform3D t3dGirarCabeza = new Transform3D();
+        Transform3D t3dMoverCabeza = new Transform3D();
+        t3dMoverCabeza.setTranslation(new Vector3f(0.38f,0.34f,0.0f));
+        t3dGirarCabeza.rotZ(Math.PI/180*-25);
+        t3dMoverCabeza.mul(t3dGirarCabeza);
+        cabeza.tgCapsula.setTransform(t3dMoverCabeza);
+        //<----FIN CABEZA---->
         
-        TransformGroup tgEsferaPanza1 = new TransformGroup(t3dEsferaPanza1);
-        Sphere esferaPanza1 = new Sphere(0.2f,apBlanca);
-        tgPanza.addChild(tgEsferaPanza1);
-        tgEsferaPanza1.addChild(esferaPanza1);
+        //<-----BASE COLA---->
+        Transform3D t3dGirarColita1 = new Transform3D();
+        Transform3D t3dMoverColita1 = new Transform3D();
+        t3dMoverColita1.setTranslation(new Vector3f(0.25f,0.45f,0.0f));
+        t3dGirarColita1.rotZ(Math.PI/180*-60);
+        t3dMoverColita1.mul(t3dGirarColita1);
+        cola1.tgCapsula.setTransform(t3dMoverColita1);
+        //<------FIN BASE COLA------->
         
-        //Esfera 2
-        Transform3D t3dEsferaPanza2 = new Transform3D();
-        t3dEsferaPanza2.setTranslation(new Vector3f(0.0f,-0.3f,0.0f));
-        
-        TransformGroup tgEsferaPanza2 = new TransformGroup(t3dEsferaPanza2);
-        Sphere esferaPanza2 = new Sphere(0.2f,apBlanca);
-        tgPanza.addChild(tgEsferaPanza2);
-        tgEsferaPanza2.addChild(esferaPanza2);
-        //<------------FIN CUERPO------------->
-        
-        //<---------------CUELLO-------------->
-        Transform3D t3dCuello = new Transform3D();
-        t3dCuello.rotZ(Math.PI/180*-30);
-        t3dCuello.setTranslation(new Vector3f(0.5f,0.335f,0.0f));
-        TransformGroup tgCuello = new TransformGroup(t3dCuello);
-        Cylinder cuello = new Cylinder(0.1f,0.2f,apBlanca);
-        objRaiz.addChild(tgCuello);
-        tgCuello.addChild(cuello);
-        
-        //esfera 1 cuello
-        Transform3D t3dEsferaCuello1 = new Transform3D();
-        t3dEsferaCuello1.setTranslation(new Vector3f(0.0f,0.1f,0.0f));
-        
-        TransformGroup tgEsferaCuello1 = new TransformGroup(t3dEsferaCuello1);
-        Sphere esferaCuello1 = new Sphere(0.1f,apBlanca);
-        tgCuello.addChild(tgEsferaCuello1);
-        tgEsferaCuello1.addChild(esferaCuello1);
-        
-        //Esfera 2 cuello
-        Transform3D t3dEsferaCuello2 = new Transform3D();
-        t3dEsferaCuello2.setTranslation(new Vector3f(0.0f,-0.1f,0.0f));
-        
-        TransformGroup tgEsferaCuello2 = new TransformGroup(t3dEsferaCuello2);
-        Sphere esferaCuello2 = new Sphere(0.1f,apBlanca);
-        tgCuello.addChild(tgEsferaCuello2);
-        tgEsferaCuello2.addChild(esferaCuello2);
-        //<------------FIN CUELLO------------->
-        
-        //<------------OREJA IZQ ------------->
-        Transform3D t3dOrejaIzq = new Transform3D();
-        
-        //<-----------FIN OREJA IZQ----------->
-            
+        //<------FINAL COLA-------->
+        Transform3D t3dGirarColita2 = new Transform3D();
+        Transform3D t3dMoverColita2 = new Transform3D();
+        t3dMoverColita2.setTranslation(new Vector3f(0.0f,0.2f,0.0f));
+        t3dGirarColita2.rotZ(Math.PI/180);
+        t3dMoverColita2.mul(t3dGirarColita2);
+        cola2.tgCapsula.setTransform(t3dMoverColita2);
+        //<------ FIN FINAL COLA-------->
         
     }
     
